3ef0082ff51f879a01447057b2bf290ee00173f4
Padma Kari <41953175+karipadma@users.noreply.github.com>
2026-09-20 11:27:52 +0530

    Update backend.tf

--- kubernetes-veeraops-ecommerce-microservices-app/EKS-Terraform/backend.tf
+++ kubernetes-veeraops-ecommerce-microservices-app/EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "Paddhubucket"
+    bucket       = "paddhubucket"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
